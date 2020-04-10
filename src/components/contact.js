c1cabaae65342a50964c5e1d83bf68353827e0e8
adevr <alex.cst.reis@gmail.com>
2020-04-10 22:25:50 +0100

    chore: edit contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,9 @@ import {Link} from "gatsby"
 
 
 const ContactWrapper = styled.div`
+  margin-bottom:0px;
   box-shadow: 0px 0px 0px -1px rgba(0,0,0,0.23);
+  background: linear-gradient(17deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
 `
 
 const IconLink = styled(Link)`
